fad4fc2c67cdebd50a904c6f33161e84d300d9ba
felix choi <felix.choi@free.fr>
2019-11-07 17:39:10 +0100

    suppression dechet

--- src/affichage.c
+++
@@ -1,9 +0,0 @@
-#include "affichage.h"
-
-void affiche_tab(int * Tab, int len_tab)
-{
-	for(int i=0; i < len_tab; i++)
-	{
-		printf("%d\t",Tab[i]);
-	}
-}
--- src/merge.h
+++
@@ -1,3 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-void merge( int * A , int * B, int * M,int len_A, int len_B );
